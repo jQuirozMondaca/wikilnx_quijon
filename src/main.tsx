b140599e8f56701a41278ed718fd874eca63b296
jQuirozMondaca <128020518+jQuirozMondaca@users.noreply.github.com>
2026-07-08 15:13:44 -0400

    fix: correccion de mayusculas en import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { WikiProvider } from "./context/WikiContext";
-import App from "./app";
+import App from "./App";
 import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
